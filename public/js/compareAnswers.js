fcedc4d0e33cc176cf05eb62f9cd014ca2142bd0
Tyler Proctor <tproctor4414@hotmail.com>
2017-12-14 14:38:48 -0800

    taking user input from form and creating a full object that is ready to post to database.  added dummy files for other front end js functionality

---
+++ public/js/compareAnswers.js
@@ -0,0 +1,5 @@
+// -get info for last beer updated by user
+// -get answers for corresponding beer
+// -write function to compare answers for each question field
+// -keep a running total of points
+// -update users new points to database
