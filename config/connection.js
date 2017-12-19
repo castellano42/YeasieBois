329f72fa639e43ce9da69f1a1d3c7d562890ae6d
Jesus Coloyan <wahoothemighty@yahoo.com>
2017-12-19 09:44:55 -0800

    fixed config

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,17 @@
 var mysql = require("mysql");
 
 var connection; 
+
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "elj742dnxqqfvwqd",
-    password: " eeouzsllhyeqs5c3",
-    database: "yeastie_boysdb"
-  });
-};
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+({
+  port: 3306,
+  host: "ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "elj742dnxqqfvwqd",
+  password: "eeouzsllhyeqs5c3",
+  database: "yeastie_boysdb"
+});
 
 // Make connection.
 connection.connect(function(err) {
